8fb01c72a28e6c7bba13619531d1119548ef70d9
ahmed <Ahmedalsoory123456@gmail.com>
2026-09-21 12:56:40 +0200

    add based classes for integration test

---
+++ Tests/Globle/IntegrationTestBase.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Shared.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Globle
+{
+    public abstract class IntegrationTestBase : IAsyncLifetime
+    {
+        protected readonly IServiceProvider _serviceProvider;
+        protected IDbContextScope _dbContextScope;
+        private bool _isTestSuccessful = false;
+
+        protected IntegrationTestBase(IntegrationTestFixture fixture)
+        {
+            _serviceProvider = fixture.ServiceProvider;
+        }
+
+        public async Task InitializeAsync()
+        {
+            _dbContextScope = _serviceProvider.GetRequiredService<IDbContextScope>();
+            await _dbContextScope.BeginTransactionAsync();
+            _isTestSuccessful = false; // Reset for each test
+        }
+
+        // Call this at the very end of your test if all assertions pass
+        protected void MarkTestAsSuccessful()
+        {
+            _isTestSuccessful = true;
+        }
+
+        public async Task DisposeAsync()
+        {
+            if (_dbContextScope != null)
+            {
+                try
+                {
+                    if (_isTestSuccessful)
+                    {
+                        // If the test reached the end successfully, commit the transaction
+                        _dbContextScope.Commit();
+                    }
+                    else
+                    {
+                        // If an assertion failed or an exception occurred, roll back
+                        _dbContextScope.Rollback();
+                    }
+                }
+                finally
+                {
+                    await _dbContextScope.DisposeAsync();
+                }
+            }
+        }
+    }
+}
---
+++ Tests/Globle/IntegrationTestFixture.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Globle
+{
+    public class IntegrationTestFixture : IAsyncLifetime
+    {
+        public IServiceProvider ServiceProvider { get; private set; }
+
+        public Task InitializeAsync()
+        {
+            var services = new ServiceCollection();
+
+            var startup = new Startup();
+            startup.ConfigureServices(services);
+
+            ServiceProvider = services.BuildServiceProvider();
+            return Task.CompletedTask;
+        }
+
+        public async Task DisposeAsync()
+        {
+            // Fix: Use IAsyncDisposable since DbContextScope implements asynchronous disposal
+            if (ServiceProvider is IAsyncDisposable asyncDisposable)
+            {
+                await asyncDisposable.DisposeAsync();
+            }
+            else if (ServiceProvider is IDisposable disposable)
+            {
+                disposable.Dispose();
+            }
+        }
+    }
+}
